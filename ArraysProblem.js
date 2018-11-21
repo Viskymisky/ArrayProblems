9c007a200f7a6382cf0d18d09f5d54477b182d28
Viskymisky <viskymisky@gmail.com>
2018-11-21 17:04:00 -0500

    Beginning of Arrays Problem

---
+++ ArraysProblem.js
@@ -0,0 +1,14 @@
+arr = [1,1,1,2]
+
+function printReverse() {
+  var i = (arr.length - 1);
+  while (i >= 0) {
+    console.log(i + ": " + arr.slice(i, i + 1));
+    i--;
+  }
+}
+
+function sumArray(){
+  var i = 0
+  for()
+}
